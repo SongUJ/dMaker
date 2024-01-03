6782f4cb01be324cc7cb9eb2ed432924f32a09cd
SongUeiJeong <uejuong@gmail.com>
2024-01-03 17:32:51 +0900

    first commit

---
+++ src/main/java/com/uj/programming/dmaker/dto/CreateDeveloper.java
@@ -0,0 +1,23 @@
+//package com.uj.programming.dmaker.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//public class CreateDeveloper {
+//
+//    @Data
+//    @AllArgsConstructor
+//    @NoArgsConstructor
+//    @Builder
+//    public static class Request {
+//        private DeveloperLevel developerLevel;
+//        private DeveloperSkillType developerSkillType;
+//        private Integer experienceYears;
+//
+//        private String memberId;
+//        private String name;
+//        private Integer age;
+//    }
+//}
